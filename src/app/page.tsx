0f63ee0f36dc8dca5124994b90ab3e6b64d40998
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-08-08 19:20:45 +0100

    feat: changed page content :tada:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const name = "Nnamdi";
-  console.log(name);
   return (
     <main className={styles.main}>
       <h1>Next.js template for commit hooks.</h1>
